2d4288f759fb831467d241119e006f6388c3d75a
Henrique <hs.ccti@gmail.com>
2013-11-10 17:01:46 -0200

    finalizando inteligencia do script

--- main.py
+++ main.py
@@ -5,43 +5,50 @@ import numpy as np
 import scipy
 from matplotlib import pyplot as plt
 from scipy import ndimage
+import os
 
 # 255 white
 # 0 black
 
-img = cv2.imread('images/image.jpg',0)
+for filename in os.listdir('images'):
+	img = cv2.imread('images/'+filename,0)
+	new_img = cv2.resize(img,(600,700))
 
-x,y = img.shape
+	x,y = img.shape
 
-filtered_img = scipy.misc.imfilter(img,'blur')
+	filtered_img = scipy.misc.imfilter(img,'blur')
 
-i = 0
-while i < int(x * 0.01):
-	filtered_img = scipy.misc.imfilter(filtered_img,'blur')
-	i += 1
 
-ret,thresh2 = cv2.threshold(filtered_img,127,255,cv2.THRESH_BINARY_INV)
+	i = 0
+	while i < int(x * 0.01):
+		filtered_img = scipy.misc.imfilter(filtered_img,'blur')
+		# print i
+		i += 1
 
-label, qtd = ndimage.label(thresh2)
+	equalizated_img = cv2.equalizeHist(filtered_img)
 
-print "(X:%i,Y:%i)" % ndimage.measurements.center_of_mass(thresh2, label)
-print qtd
+	ret,thresh2 = cv2.threshold(equalizated_img,127,255,cv2.THRESH_BINARY_INV)
 
-i = 0
-while i < x:
-	for b in thresh2[i]:
-		if b == 0:
-			break
-		thresh2[i][np.where(thresh2[i] == b)[0][0]] = 0
-	i += 1
+	label, qtd = ndimage.label(thresh2)
 
-i = 0
-while i < len(thresh2):
-	for b in thresh2[i][::-1]:
-		if b == 0:
-			break
-		thresh2[i][::-1][np.where(thresh2[i][::-1] == b)[0][0]] = 0
-	i += 1		
+	i = 0
+	while i < x:
+		for b in thresh2[i]:
+			if b == 0:
+				break
+			thresh2[i][np.where(thresh2[i] == b)[0][0]] = 0
+		i += 1
 
-plt.imshow(thresh2, cmap=plt.cm.gray)
-plt.show()
+	i = 0
+	while i < len(thresh2):
+		for b in thresh2[i][::-1]:
+			if b == 0:
+				break
+			thresh2[i][::-1][np.where(thresh2[i][::-1] == b)[0][0]] = 0
+		i += 1
+
+	print filename + "- (X:%i,Y:%i)" % ndimage.measurements.center_of_mass(thresh2, label)
+	print qtd
+
+# plt.imshow(thresh2, cmap=plt.cm.gray)
+# plt.show()
