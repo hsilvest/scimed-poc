e5aa76e50cba8ab97855cdc66080b3ca9a50aa0e
Henrique <hs.ccti@gmail.com>
2013-11-17 21:55:45 -0200

    criacao de interface e adequacao do algoritmo de deteccao

--- gui.py
+++ gui.py
@@ -1,59 +1,3 @@
-# Author: Henrique Silvestre Souza
-
-import cv2
-import numpy as np
-import scipy
-from matplotlib import pyplot as plt
-from scipy import ndimage
-import os
-
-def image_detect(file):
-	# 255 white
-	# 0 black
-
-	# for filename in os.listdir('images'):
-	# img = cv2.imread('images/'+filename,0)
-	img = file
-	new_img = cv2.resize(img,(600,700))
-
-	x,y = img.shape
-
-	filtered_img = scipy.misc.imfilter(img,'blur')
-
-
-	i = 0
-	while i < int(x * 0.01):
-		filtered_img = scipy.misc.imfilter(filtered_img,'blur')
-		# print i
-		i += 1
-
-	equalizated_img = cv2.equalizeHist(filtered_img)
-
-	ret,thresh2 = cv2.threshold(equalizated_img,127,255,cv2.THRESH_BINARY_INV)
-
-	label, qtd = ndimage.label(thresh2)
-
-	i = 0
-	while i < x:
-		for b in thresh2[i]:
-			if b == 0:
-				break
-			thresh2[i][np.where(thresh2[i] == b)[0][0]] = 0
-		i += 1
-
-	i = 0
-	while i < len(thresh2):
-		for b in thresh2[i][::-1]:
-			if b == 0:
-				break
-			thresh2[i][::-1][np.where(thresh2[i][::-1] == b)[0][0]] = 0
-		i += 1
-
-	print filename + "- (X:%i,Y:%i)" % ndimage.measurements.center_of_mass(thresh2, label)
-	print qtd
-
-	# plt.imshow(thresh2, cmap=plt.cm.gray)
-	# plt.show()
 
 title = 'X-Ray Hipodisease Detect'
 
@@ -125,7 +69,6 @@ class Demo:
 		padx = 60,
 		pady = 60)
 	self.mainPart.pack(fill='both',expand=1)
-	
 class PrintOne:
 	def __init__(self,text):
 		self.text=text
@@ -146,3 +89,5 @@ root.destroy)
     exitButton.pack(side = 'bottom')
     widget = Demo(root)
     root.mainloop()
+
+
